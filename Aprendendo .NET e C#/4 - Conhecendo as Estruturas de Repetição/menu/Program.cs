c185433810b1288c551d8aacf8a0a0a1ba920251
the-paiva <hrpaiva3@gmail.com>
2024-10-22 14:13:39 -0300

    Códigos da etapa 'Conhecendo estruturas de repetição' adicionados

---
+++ Aprendendo .NET e C#/4 - Conhecendo as Estruturas de Repetição/menu/Program.cs
@@ -0,0 +1,41 @@
+﻿// Arquivo feito especificamente para a prática do menu interativo da etapa "Conhecendo as estruturas de repetição" do curso de C#
+
+int input;
+bool exibirOpcoes = true;
+
+while (exibirOpcoes)
+{
+    Console.Clear(); // Limpa a tela
+    Console.WriteLine("Digite uma das opções abaixo");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Sair");
+    Console.Write("> ");
+    input = Convert.ToInt32(Console.ReadLine());
+
+    switch (input)
+    {
+        case 1:
+            Console.WriteLine("\nA opção de cadastro de cliente foi escolhida");
+            Thread.Sleep(5000); // Bloqueia a execução do programa durante 5 segundos
+            break;
+        case 2:
+            Console.WriteLine("\nA opção de buscar cliente foi escolhida");
+            Thread.Sleep(5000);
+            break;
+        case 3:
+            Console.WriteLine("\nA opção de apagar cliente foi escolhida");
+            Thread.Sleep(5000);
+            break;
+        case 4:
+            Console.WriteLine("\nEncerrando o programa...");
+            Thread.Sleep(3000); 
+            exibirOpcoes = false; // Encerra o programa
+            break;
+        default:
+            Console.WriteLine("\nOPÇÃO INVÁLIDA!");
+            Thread.Sleep(5000);
+            break;
+    }
+}
---
+++ Aprendendo .NET e C#/4 - Conhecendo as Estruturas de Repetição/projetinho/Program.cs
@@ -0,0 +1,83 @@
+﻿// Código referente à etapa "Conhecendo Estruturas de Repetição" do curso de C#
+
+//===========================================================================================================================================
+//                                                          Uso do FOR
+//===========================================================================================================================================
+
+// Exemplo: Tabuada de multiplacação do número 3
+
+int numero = 3;
+
+Console.WriteLine("Tabuada de multiplicação do 3 (Usando FOR)");
+
+// Estrutura básica do FOR
+for (int contFor = 0; contFor <= 10; contFor++)
+{
+    Console.WriteLine($"{numero} * {contFor} = {numero * contFor}");
+}
+
+
+//===========================================================================================================================================
+//                                                          Uso do WHILE
+//===========================================================================================================================================
+
+// Exemplo: Tabuada de multiplicação do número 5
+
+numero = 5;
+int contWhile = 0;
+
+Console.WriteLine("\nTabuada de multiplicação do 5 (Usando WHILE)");
+
+// Estrutura básica do WHILE
+while (contWhile <= 10)
+{
+    Console.WriteLine($"{numero} * {contWhile} = {numero * contWhile}");
+    contWhile++;
+}
+
+// Exemplo: Tabuada de multiplicação do número 7
+
+numero = 7;
+contWhile = 0;
+
+Console.WriteLine("\nTabuada de multiplicação do 7 (Usando WHILE e BREAK)");
+
+// Estrutura do WHILE usando BREAK
+while (contWhile <= 10)
+{
+    Console.WriteLine($"{numero} * {contWhile} = {numero * contWhile}");
+
+    if (contWhile == 5)
+    {
+        break;
+    }
+
+    contWhile++;
+}
+
+//===========================================================================================================================================
+//                                                          Uso de DO WHILE
+//===========================================================================================================================================
+
+// Exemplo: Soma iterativa
+
+int novoNumero, soma = 0, contDo = 1;
+
+Console.WriteLine("\nSoma iterativa (Usando Do While) - Digite 0 para encerrar");
+
+do
+{
+    Console.Write($"Digite o {contDo}º número: ");
+    novoNumero = Convert.ToInt32(Console.ReadLine());
+
+    if (novoNumero == 0)
+    {
+        break;
+    }
+
+    soma += novoNumero;
+    contDo++;
+} 
+while (true);
+
+Console.WriteLine($"\nSoma dos números digitados: {soma}");
